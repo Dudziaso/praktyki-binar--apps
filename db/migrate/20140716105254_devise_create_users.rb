7ecfdcc5abc64464ab254be6041e8dac9b2edbf0
Artur Dudzik <dudziaso@gmail.com>
2014-07-18 13:21:51 +0200

    poprawienie migracji

--- db/migrate/20140716105254_devise_create_users.rb
+++ db/migrate/20140716105254_devise_create_users.rb
@@ -9,8 +9,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.integer  :sign_in_count, default: 0, null: false
       t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at
-      t.stri
-      ng   :current_sign_in_ip
+      t.string   :current_sign_in_ip
       t.string   :last_sign_in_ip
 
       ## Confirmable
